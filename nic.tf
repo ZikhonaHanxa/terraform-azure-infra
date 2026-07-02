339d4099782c23fff25ceeac335fab5f35ca604b
ZikhonaHanxa <138576941+ZikhonaHanxa@users.noreply.github.com>
2026-07-02 12:19:57 +0200

    Update nic.tf

--- nic.tf
+++ nic.tf
@@ -3,9 +3,9 @@ resource "azurerm_network_interface" "nic" {
     location = var.location
     resource_group_name = azurerm_resource_group.MyRG.name
     ip_configuration {
-      name = myNicConfiguration
-      subnet_id = abs()
-      private_ip_address_allocation = abs()
-      public_ip_address_id = 
+      name = "myNicConfiguration"
+      subnet_id = azurerm_subnet.mysubnet.id
+      private_ip_address_allocation = "Dynamic"
+      public_ip_address_id = azurerm_public_ip.PubIP.id
     }
 }
