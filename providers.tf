de6131396f0f6e0b9e49ba89d809198caf2fb529
ZikhonaHanxa <138576941+ZikhonaHanxa@users.noreply.github.com>
2026-07-06 12:53:50 +0200

    Adding random provider

--- providers.tf
+++ providers.tf
@@ -4,9 +4,16 @@ terraform {
         source = "hashicorp/azurerm"
         version ="4.79.0"
     }
+    random={
+      source = "hashicorp/random"
+      version = "3.9.0"
+    }
   }
 }
 provider "azurerm"   {
     features{}
+  
+}
+provider "random" {
   
 }
